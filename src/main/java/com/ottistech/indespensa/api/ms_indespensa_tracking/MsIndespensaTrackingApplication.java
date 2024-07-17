a9423f771ac52777feca9b7ee585439f58a79bc4
pedroggwp <phsnnascimento@outlook.com>
2024-07-17 12:55:42 -0300

    Implementação de um controller simples que retorna Hello World

--- src/main/java/com/ottistech/indespensa/api/ms_indespensa_tracking/MsIndespensaTrackingApplication.java
+++ src/main/java/com/ottistech/indespensa/api/ms_indespensa_tracking/MsIndespensaTrackingApplication.java
@@ -1,7 +1,15 @@
 package com.ottistech.indespensa.api.ms_indespensa_tracking;
 
+import lombok.extern.java.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class MsIndespensaTrackingApplication {
@@ -10,4 +18,13 @@ public class MsIndespensaTrackingApplication {
 		SpringApplication.run(MsIndespensaTrackingApplication.class, args);
 	}
 
+	@RestController
+	@RequestMapping("/api/v1")
+	public class HelloWorld {
+
+		@GetMapping("/hello")
+		public String hello() {
+			return "Hello World";
+		}
+	}
 }
