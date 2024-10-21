578e8660313408a0fbd8d2ea33a2edd134409b84
pedroggwp <phsnnascimento@outlook.com>
2024-10-21 07:54:07 -0300

    removing and changing ms-indespensa-products-recommendation

--- src/main/java/com/ottistech/indespensa/api/ms_indespensa_products_recommendation/MsIndespensaProductsRecommendationApplication.java
+++ src/main/java/com/ottistech/indespensa/api/ms_indespensa_products_recommendation/MsIndespensaProductsRecommendationApplication.java
@@ -2,8 +2,10 @@ package com.ottistech.indespensa.api.ms_indespensa_products_recommendation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.ottistech.indespensa.api.ms_indespensa_products_recommendation.repository")
 public class MsIndespensaProductsRecommendationApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/ottistech/indespensa/api/ms_indespensa_products_recommendation/MsIndespensaProductRecommendationApplication.java
+++ src/test/java/com/ottistech/indespensa/api/ms_indespensa_products_recommendation/MsIndespensaProductRecommendationApplication.java
@@ -2,10 +2,8 @@ package com.ottistech.indespensa.api.ms_indespensa_products_recommendation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackages = "com.ottistech.indespensa.api.ms_indespensa_products_recommendation.repository")
 public class MsIndespensaProductRecommendationApplication {
 
 	public static void main(String[] args) {
